a15b63bb28d7cb00b15576c41cd90f2479512544
Gurpreet Singh <Singhamarjot9@yahoo.com>
2023-09-25 03:06:42 +0000

    added bootstrap elements

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser')
+var axios = require('axios').default
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
@@ -8,9 +9,13 @@ app.use('/static', express.static('public'));
 app.set('view engine', 'ejs')
 
 app.get('/', function(req, res) {
-    res.render('home.ejs', {name: null});
+    axios.get('https://xkcd.com/info.0.json').then(function(response){
+        console.log(response.data)
+        res.render('home.ejs', {name: null, xkcdData: response.data});
+    })
 })
 
+/*
 app.get('/path/:name', function(req, res) {
     let name = req.params.name;
     res.render('home.ejs', {name: name});
@@ -23,11 +28,12 @@ app.get('/query', function(req, res) {
 })
 
 app.post('/create', (req, res) =>{
-    console.log(req.body.email)
+   console.log(req.body.email)
     res.redirect('/')
 })
 
 //app.put(route, function)
+*/
 
 app.listen(3000, () => {
     console.log('started on port 3000');
